e6557ab56ae6d93920f21984f22ffa6e075d5892
Sheyla Ixtabalan Argueta <sheylaixtabalanargueta@Sheylas-MacBook-Pro.local>
2026-06-16 14:16:55 -0700

    Add Python profile variables lab

---
+++ profile.py
@@ -0,0 +1,16 @@
+full_name = "Sheyla Ixtabalan Argueta"
+print(full_name)
+nickname = "Shey"
+print(nickname)
+age = 18
+print(age)
+used_python_before = False
+print(used_python_before)
+hobbies = ["soccer" , "cooking"]
+print(hobbies)
+favorites = {
+    "movie": "coco" , 
+    "food": "chipotle" , 
+    "hobby": "soccer"
+}
+print(favorites)
